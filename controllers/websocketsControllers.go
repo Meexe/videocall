edadd86d0b322fa1eeb4048092f9f4d4e02053df
Maxim Pak <maxpak99@gmail.com>
2020-06-09 05:07:30 +0300

    fixed websockets not closing bug

--- controllers/websocketsControllers.go
+++ controllers/websocketsControllers.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"log"
 	"net/http"
+	"reflect"
 	"sync"
 	"time"
 
@@ -33,26 +34,65 @@ func (onlineUsers *Users) GetOnlineUsers(w http.ResponseWriter, r *http.Request)
 	user := models.GetUser(userID)
 	log.Printf("User %s connected\n", user.Nickname)
 	onlineUsers.Lock()
-	onlineUsers.Users[user.Nickname] = true // append(onlineUsers.users, user.Nickname)
+	onlineUsers.Users[user.Nickname] = true
 	onlineUsers.Unlock()
 
-	for {
-		users := make([]string, len(onlineUsers.Users)-1)
-		index := 0
-		for key := range onlineUsers.Users {
-			if key == user.Nickname {
-				continue
+	var oldstate []string
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	defer wg.Wait()
+
+	go func() {
+		var msg string
+		defer wg.Done()
+		for {
+			err := ws.ReadJSON(msg)
+			if err != nil {
+				log.Println("read:", err)
+				log.Printf("User %s disconnected\n", user.Nickname)
+				onlineUsers.Lock()
+				delete(onlineUsers.Users, user.Nickname)
+				onlineUsers.Unlock()
+				return
 			}
-			users[index] = key
-			index++
 		}
-		err := ws.WriteJSON(users)
-		if err != nil {
-			log.Println("write:", err)
-			log.Printf("User %s disconnected\n", user.Nickname)
-			delete(onlineUsers.Users, user.Nickname)
-			break
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			index := 0
+			flag := false
+
+			onlineUsers.Lock()
+			users := make([]string, len(onlineUsers.Users))
+			for key := range onlineUsers.Users {
+				if key == user.Nickname {
+					flag = true
+					continue
+				}
+				users[index] = key
+				index++
+			}
+			onlineUsers.Unlock()
+
+			if !flag {
+				return
+			}
+
+			if !reflect.DeepEqual(oldstate, users) {
+				oldstate = users
+				err := ws.WriteJSON(users[:len(users)-1])
+				if err != nil {
+					log.Println("write:", err)
+					log.Printf("User %s disconnected\n", user.Nickname)
+					onlineUsers.Lock()
+					delete(onlineUsers.Users, user.Nickname)
+					onlineUsers.Unlock()
+					return
+				}
+			}
+			time.Sleep(5 * time.Second)
 		}
-		time.Sleep(10 * time.Second)
-	}
+	}()
 }
